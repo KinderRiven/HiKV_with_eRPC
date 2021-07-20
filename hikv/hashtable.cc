381bb1b5f700247d8200788f14d9e507bf47e6d7
KinderRiven <1339764596@qq.com>
2021-07-20 16:13:25 +0800

    simple push

--- hikv/hashtable.cc
+++ hikv/hashtable.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-07-20 14:28:38
- * @LastEditTime: 2021-07-20 16:10:23
+ * @LastEditTime: 2021-07-20 16:13:11
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /HiKV+++/hikv/hashtable.cc
@@ -16,6 +16,7 @@ HashTable::HashTable(const Options& options, Allocator* allocator)
     size_t _index_size = options.index_size / options.num_server_threads;
     table_ = (hash_bucket_t*)allocator->AlignAllocate(256, _index_size);
     printf("HashTable[SIZE:%.2fGB][ADDR:%llu]\n", 1.0 * _index_size / (1024 * 1024 * 1024), (uint64_t)table_);
+    memset(table_, 0, _index_size);
 }
 
 HashTable::~HashTable()
