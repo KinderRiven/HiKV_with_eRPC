e2f00b48d06fec1e2ded3030c3bda19a35eea9b9
KinderRiven <1339764596@qq.com>
2021-07-22 19:43:52 +0800

    simple push

--- benchmark/cs/server.cc
+++ benchmark/cs/server.cc
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2021-04-08 10:36:18
- * @LastEditTime: 2021-07-22 13:08:41
- * @LastEditors: your name
+ * @LastEditTime: 2021-07-22 19:40:35
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /code/eRPC/hello_world/server.cc
  */
@@ -14,7 +14,6 @@ void req_handler(erpc::ReqHandle* req_handle, void*)
     auto& resp = req_handle->pre_resp_msgbuf;
     rpc->resize_msg_buffer(&resp, kMsgSize);
     sprintf(reinterpret_cast<char*>(resp.buf), "hello");
-
     rpc->enqueue_response(req_handle, &resp);
 }
 
@@ -23,7 +22,6 @@ int main()
     std::string server_uri = kServerHostname + ":" + std::to_string(kUDPPort);
     erpc::Nexus nexus(server_uri, 0, 0);
     nexus.register_req_func(kReqType, req_handler);
-
     rpc = new erpc::Rpc<erpc::CTransport>(&nexus, nullptr, 0, nullptr);
     rpc->run_event_loop(100000);
 }
