d86be369980fdf982977c028d70fd03cc9984b7c
KinderRiven <1339764596@qq.com>
2021-07-23 10:10:00 +0800

    simple push

--- benchmark/cs/common.h
+++ benchmark/cs/common.h
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "rpc.h"
 
-static const std::string kServerHostname = "172.18.13.42";
-static const std::string kClientHostname = "172.18.13.40";
+static const std::string kServerHostname = "10.0.0.40";
+static const std::string kClientHostname = "10.0.0.42";
 
 static constexpr uint16_t kUDPPort = 31850;
 static constexpr uint8_t kReqType = 2;
