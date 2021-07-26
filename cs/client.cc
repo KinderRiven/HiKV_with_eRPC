2fb0567f051b08a59817225bba967f61022b116d
KinderRiven <1339764596@qq.com>
2021-07-26 19:19:55 +0800

    simple push

--- cs/client.cc
+++ cs/client.cc
@@ -1,12 +1,15 @@
 /*
  * @Author: your name
  * @Date: 2021-04-08 10:36:18
- * @LastEditTime: 2021-07-26 18:56:58
+ * @LastEditTime: 2021-07-26 19:19:13
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /HiKV+++/benchmark/cs/client.cc
  */
 #include "common.h"
+
+#define REQ_INSERT (1)
+#define REQ_SEARCH (2)
 
 struct RequestContext {
 public:
@@ -14,6 +17,8 @@ public:
     int complete;
     size_t start_time;
     size_t end_time;
+    erpc::MsgBuffer req;
+    erpc::MsgBuffer resp;
 };
 
 struct ResultContext {
@@ -35,8 +40,6 @@ public:
 
     erpc::Nexus* nexus;
     erpc::Rpc<erpc::CTransport>* rpc;
-    erpc::MsgBuffer req;
-    erpc::MsgBuffer resp;
 
     RequestContext request;
     ResultContext result;
@@ -56,8 +59,20 @@ void kv_cont_func(void* context, void* tag)
 {
     ClientContext* _context = (ClientContext*)context;
     _context->request.end_time = erpc::rdtsc();
-    _context->request.complete = 1;
-    _context->result.num_insert_ok++;
+    if (_context->type == REQ_INSERT) {
+        _context->result.num_insert_ok++;
+        _context->request.complete = 1;
+    } else if (_context->Type == REQ_SEARCH) {
+        char* _buf = _context->request.resp.buf;
+        uint64_t _num_kv = *(uint64_t*)_buf;
+        _buf += kHeadSize;
+        uint64_t _key = *(uint64_t*)_buf;
+        _buf += kKeySize;
+        uint64_t _value = *(uint64_t*)_buf;
+        _context->result.num_search_ok++;
+        _context->request.complete = 1;
+        printf("%llu - %llu\n", _key, _value);
+    }
 }
 
 static void run_client_thread(ClientContext* context)
@@ -74,12 +89,14 @@ static void run_client_thread(ClientContext* context)
         _rpc->run_event_loop_once();
     }
     printf("[%d][Connect Finished]\n", _thread_id);
-    context->req = _rpc->alloc_msg_buffer_or_die(kMsgSize);
-    context->resp = _rpc->alloc_msg_buffer_or_die(kMsgSize);
+    context->request.req = _rpc->alloc_msg_buffer_or_die(kMsgSize);
+    context->request.resp = _rpc->alloc_msg_buffer_or_die(kMsgSize);
 
+    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     uint64_t _base = context->base;
     size_t _start_time = erpc::rdtsc();
     for (uint64_t i = 1; i <= context->num_kv; i++) {
+        context->request.type = REQ_INSERT;
         context->request.complete = 0;
         // +++++++++++++++++++++++++++++++++++++++
         char* __dest = (char*)context->req.buf;
@@ -90,7 +107,7 @@ static void run_client_thread(ClientContext* context)
         *(uint64_t*)__dest = _base;
         context->request.start_time = erpc::rdtsc();
         // +++++++++++++++++++++++++++++++++++++++
-        _rpc->enqueue_request(_session_num, kInsertType, &context->req, &context->resp, kv_cont_func, nullptr);
+        _rpc->enqueue_request(_session_num, kInsertType, &context->request.req, &context->request.resp, kv_cont_func, nullptr);
         while (!context->request.complete) {
             _rpc->run_event_loop_once();
         }
@@ -99,9 +116,40 @@ static void run_client_thread(ClientContext* context)
     }
     size_t _end_time = erpc::rdtsc();
     double _lat = erpc::to_usec(_end_time - _start_time, _rpc->get_freq_ghz());
-    printf("[%d][%llu][time:%.2fseconds][iops:%.2f]\n",
+    printf("[insert][%d][%llu][time:%.2fseconds][iops:%.2f]\n",
         _thread_id, context->result.num_insert_ok,
         _lat / 1000000.0, 1.0 * context->result.num_insert_ok / (_lat / 1000000.0));
+    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    rpc->resize_msg_buffer(&context->request.req, kHeadSize + kKeySize);
+    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    _base = context->base;
+    _start_time = erpc::rdtsc();
+    for (uint64_t i = 1; i <= context->num_kv; i++) {
+        context->request.type = REQ_SEARCH;
+        context->request.complete = 0;
+        // +++++++++++++++++++++++++++++++++++++++
+        char* __dest = (char*)context->req.buf;
+        *(uint64_t*)__dest = 1;
+        __dest += kHeadSize;
+        *(uint64_t*)__dest = _base;
+        __dest += kKeySize;
+        *(uint64_t*)__dest = _base;
+        context->request.start_time = erpc::rdtsc();
+        // +++++++++++++++++++++++++++++++++++++++
+        _rpc->enqueue_request(_session_num, kSearchType, &context->request.req, &context->request.resp, kv_cont_func, nullptr);
+        while (!context->request.complete) {
+            _rpc->run_event_loop_once();
+        }
+        _base++;
+        // +++++++++++++++++++++++++++++++++++++++
+    }
+    _end_time = erpc::rdtsc();
+    _lat = erpc::to_usec(_end_time - _start_time, _rpc->get_freq_ghz());
+    printf("[search][%d][%llu][time:%.2fseconds][iops:%.2f]\n",
+        _thread_id, context->result.num_insert_ok,
+        _lat / 1000000.0, 1.0 * context->result.num_insert_ok / (_lat / 1000000.0));
+    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 }
 
 int main()
