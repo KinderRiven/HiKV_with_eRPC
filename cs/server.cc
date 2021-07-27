88438c318a8dc68b875093e6b48fb6b97cdc3ce1
KinderRiven <1339764596@qq.com>
2021-07-27 12:50:17 +0800

    simple push

--- cs/server.cc
+++ cs/server.cc
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-04-08 10:36:18
- * @LastEditTime: 2021-07-27 12:41:38
+ * @LastEditTime: 2021-07-27 12:50:04
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /code/eRPC/hello_world/server.cc
@@ -10,6 +10,9 @@
 #include "common.h"
 #include "hikv.hpp"
 #include <thread>
+
+static int g_numa[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29 };
+// static int g_numa[] = { 11, 12, 13, 14, 15, 16, 17, 18, 19, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39 };
 
 struct ServerContext {
 public:
@@ -86,6 +89,14 @@ void req_search_handle(erpc::ReqHandle* req_handle, void* context)
 static void run_server_thread(ServerContext* context)
 {
     int _thread_id = context->thread_id;
+#if 1
+    cpu_set_t _mask;
+    CPU_ZERO(&_mask);
+    CPU_SET(g_numa[thread_id], &_mask);
+    if (pthread_setaffinity_np(pthread_self(), sizeof(_mask), &_mask) < 0) {
+        printf("threadpool, set thread affinity failed.\n");
+    }
+#endif
     erpc::Nexus* _nexus = context->nexus;
     printf("CreateRPC - %d\n", _thread_id);
     context->rpc = new erpc::Rpc<erpc::CTransport>(_nexus, (void*)context, _thread_id, sm_handler);
